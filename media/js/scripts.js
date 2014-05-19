46813b15ec07b5fdd65b3de761fe7efb1e00bf37
Joey Pereira <joey@joeypereira.com>
2014-05-19 14:55:20 -0400

    no longer used libraries! so many

--- media/js/scripts.js
+++
@@ -1,9 +0,0 @@
-function gen_mail_to_link(lhs,rhs) {
-    document.write("<A HREF=\"mailto");
-    document.write(":" + lhs + "@");
-    document.write(rhs + "\">" + lhs + "@" + rhs + "<\/A>");
-}
-
-function email() {
-  gen_mail_to_link('joey','joeypereira.com');
-}
